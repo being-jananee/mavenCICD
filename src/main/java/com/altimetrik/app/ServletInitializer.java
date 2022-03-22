ea6653b3f4b3798301f8b425b93f728e463de7e6
ShreeJananee Venkatraman <svenkatraman@altimetrik.com>
2022-03-22 16:32:27 +0530

    Initial Commit

---
+++ src/main/java/com/altimetrik/app/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.altimetrik.app;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CicdDemo1Application.class);
+	}
+
+}
